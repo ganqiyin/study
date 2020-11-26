050ad7bec176dd382224dfeb6bcef9528e6bf7f9
ganqiyin <ganqiyin2009@163.com>
2020-11-26 15:37:00 +0800

    添加 退出按钮

--- IdentityServer4/GR.Web/Program.cs
+++ IdentityServer4/GR.Web/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace GR.Web
 {
@@ -20,7 +15,9 @@ namespace GR.Web
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                     .UseSerilog((ctx, cfg) => cfg
+                     .ReadFrom.Configuration(ctx.Configuration));
                 });
     }
 }
--- IdentityServer4/GR.Web/Startup.cs
+++ IdentityServer4/GR.Web/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace GR.Web
 {
@@ -42,7 +44,7 @@ namespace GR.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -54,6 +56,7 @@ namespace GR.Web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            loggerFactory.AddSerilog();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
